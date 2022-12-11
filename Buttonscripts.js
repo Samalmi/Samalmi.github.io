b3972d4625d425383e18c67880d0782e8ca7280d
Samalmi <samalmi2001@icloud.com>
2022-12-10 17:14:44 -0800

    Add files via upload

---
+++ Buttonscripts.js
@@ -0,0 +1,10 @@
+//Made by Samalmi for her personal website
+//2022-12-06 - Last edit 2022-12-06
+//home page javascript
+
+<
+script >
+    function doSomething() {
+        alert('Did something');
+    } <
+    /script>
